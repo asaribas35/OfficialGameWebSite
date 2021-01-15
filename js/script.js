1814a915eb467307e7e782cdb32a16a17dd0f9aa
asaribas35 <asaribas3535@gmail.com>
2021-01-15 08:29:50 +0300

    jquery kodları script.js ye tasındı

--- js/script.js
+++ js/script.js
@@ -1,24 +1,16 @@
-const but1 =document.getElementById("num1")
-const but2 =document.getElementById("num2")
-const but3 =document.getElementById("num3")
+//--------------------- OWL CAROUSEL 
 
-but1.addEventListener("click",function sek1(){
-document.querySelector("body > div > div.kayar1").classList.add("pin1");
-document.querySelector("body > div > div.kayar2").classList.remove("pin1");
-document.querySelector("body > div > div.kayar3").classList.remove("pin1");
-setTimeout(sek2 ,1000)
-});
-but2.addEventListener("click",function sek2(){
-    document.querySelector("body > div > div.kayar2").classList.add("pin1");
-    document.querySelector("body > div > div.kayar1").classList.remove("pin1");
-    document.querySelector("body > div > div.kayar3").classList.remove("pin1");
-    
-});
- but3.addEventListener("click",function sek3(){
-    document.querySelector("body > div > div.kayar3").classList.add("pin1");
-    document.querySelector("body > div > div.kayar2").classList.remove("pin1");
-    document.querySelector("body > div > div.kayar1").classList.remove("pin1");
-});
+$(document).ready(function(){
+    let owl= $(".owl-carousel").owlCarousel({
+         loop:true,
+        dots:false,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+          autoplayHoverPause:true
+    }
+        
+    );
+   });
 
-
-
+//--------------------- OWL CAROUSEL 
